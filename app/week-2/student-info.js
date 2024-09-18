e3b1a266370f63b30eb99b9830785a36829e9989
Gurpesh <gurpesh.singh@edu.sait.ca>
2024-09-18 08:46:39 -0600

    Added week 2 page
    Made changes to week 2 page and root app page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1>Gurpesh Singh</h1>
+            <p><Link href= "https://github.com/Gurpesh/cprg306-assignments">Gurpesh</Link></p>
+        </div>
+    );
+}
+export default StudentInfo;
