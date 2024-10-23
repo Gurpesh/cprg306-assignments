4ebe732c2daa7b517022b0d206c3951c80af82fb
Gurpesh <gurpesh.singh@edu.sait.ca>
2024-10-23 16:39:17 -0600

    did week 7
    Added counter in the page

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Page() {
         <p>Click on the link to go to <Link href="/week-4">week-4 page</Link>.</p>
         <p>Click on the link to go to <Link href="/week-5">week-5 page</Link>.</p>
         <p>Click on the link to go to <Link href="/week-6">week-6 page</Link>.</p>
+        <p>Click on the link to go to <Link href="/week-7">week-7 page</Link>.</p>
       </div>
     );
 
